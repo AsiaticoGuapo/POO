f45a133cecc00a3bf2525e14e310978c328a1f6a
chino <aeqv226@gmail.co>
2018-07-09 22:48:29 -0600

    radio

---
+++ src/Radio.kt
@@ -0,0 +1,60 @@
+class Radio(    var encendido: Boolean = false,
+                var volumen: Int = 0,
+                var estacion: Double = 0.0,
+                var sintonia: String = "Fm") {
+    fun subirVolumen(int: Int){
+        volumen =+ int
+        if( volumen > 100){
+            volumen = 100
+        }
+
+    }
+    fun bajarVolumen(int: Int){
+        volumen =- int
+        if(volumen < 0){
+            volumen = 0
+        }
+    }
+    fun cambiarEstacion(d: Double){
+        estacion = d
+    }
+
+    fun cambiarSintonia(){ if (sintonia.equals("Fm")){
+        sintonia = "Am"
+    }else{
+        sintonia = "Fm"
+    }}
+    fun encenderApagar(){
+        if(encendido == false){
+            println("Acaba de encender la radio")
+            encendido = true
+        }else{
+            println("Acaba de apagar la radio")
+            encendido = false
+        }
+
+    }
+
+    override fun toString(): String {
+        return """
+            Estado actual de la radio:
+                Estacion: $estacion
+                Fm/Am: $sintonia
+                Volumen: $volumen
+                Encendido/Apagado: $encendido
+        """.trimIndent()
+    }
+
+    fun menu():String{
+        return """
+            ¿Que opcion desea?
+            Ingrese el numero de la opcion que quiere(1-6)
+            1.Cambiar de sintonia
+            2.Subir volumen
+            3.Bajar volumen
+            4.Cambiar FM/AM
+            5.Encender o apagar la radio
+            6.Exit
+            """.trimIndent()
+    }
+}
--- src/main/RadioKotlin.kt
+++
@@ -1,9 +0,0 @@
-package main
-
-fun main(args : Array<String>){
-    println("Creando la radio en Kotlin")
-    println("Tratando de hace un commint")
-
-}
-
-
