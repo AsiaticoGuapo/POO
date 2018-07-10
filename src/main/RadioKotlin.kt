38b524cac38abe1ff20ddb73bf62b1b6640f754b
chino <aeqv226@gmail.co>
2018-07-09 21:51:06 -0600

    radio

---
+++ src/main/RadioKotlin.kt
@@ -0,0 +1,9 @@
+package main
+
+fun main(args : Array<String>){
+    println("Creando la radio en Kotlin")
+    println("Tratando de hace un commint")
+
+}
+
+
--- src/main/utils/radio2.kt
+++ src/main/utils/radio2.kt
@@ -1,2 +1,26 @@
-package main
+package main.utils
+class Car(
+        private val model: Int,
+        private val color: String,
+        var isTurnedOn: Boolean = false,
+        private val numberOfDoors: Int = 2
+) {
+    fun turnOn() {
+        isTurnedOn = true
+    }
+
+    fun turnOff() {
+        isTurnedOn = false
+    }
+
+    override fun toString(): String {
+        return """
+            Car:
+                Model: $model
+                Color: $color
+                Doors: $numberOfDoors
+                Is turned On: $isTurnedOn
+        """.trimIndent()
+    }
+}
 
