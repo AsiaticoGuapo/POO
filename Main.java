434623de30ea9bfc08ddb24b489941626deb2891
AsiaticoGuapo <40869786+AsiaticoGuapo@users.noreply.github.com>
2018-07-16 22:33:08 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+//AndresQuinto 18288
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner input;
+
+    public static void menu() {
+        System.out.println("Menu Principal:\n\t1.Add Nivel\n\t2.Add Grado\n\t3.Add Estudiante\n\t4.Mostrar grados en un Nivel\n\t5.Mostrar asignaciones en un Grado\n\t6.close");
+    }
